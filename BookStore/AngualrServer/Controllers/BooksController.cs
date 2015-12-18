281224cd77462231b6acdc9e5e2dbd46222c844a
Arkadiusz Pisarek <Arkadiusz Pisarek>
2015-12-18 18:01:12 +0100

    angualr

---
+++ BookStore/AngualrServer/Controllers/BooksController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace AngualrServer.Controllers
+{
+    [Route("api/[controller]")]
+    public class BooksController : Controller
+    {
+        // GET: api/books
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
