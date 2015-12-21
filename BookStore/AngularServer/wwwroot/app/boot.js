08e4a8d9b2e61fe7c085ba0395f2385ebf4df636
Pawel <pisarekp73@gmail.com>
2015-12-21 11:28:27 +0100

    edit readme

---
+++ BookStore/AngularServer/wwwroot/app/boot.js
@@ -0,0 +1,4 @@
+var browser_1 = require('angular2/platform/browser');
+var http_1 = require('angular2/http');
+var app_component_1 = require('./app.component');
+browser_1.bootstrap(app_component_1.AppComponent, [http_1.HTTP_PROVIDERS]);
